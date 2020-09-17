521002b57c9ada3808d2cd1a1f5c7fdec0fdd816
susi-101 <71079673+susi-101@users.noreply.github.com>
2020-09-17 22:02:32 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mycompany.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+}
